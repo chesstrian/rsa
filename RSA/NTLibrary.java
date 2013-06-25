f49665eebc40fb15162944d8e6888cd9e651fe1f
Christian Gutierrez <chesstrian@gmail.com>
2013-06-25 09:50:31 -0500

    Cleaning tests.

--- RSA/NTLibrary.java
+++ RSA/NTLibrary.java
@@ -5,9 +5,8 @@ package RSA;
  * @author chesstrian
  */
 public class NTLibrary {
-    public static String int2bin(int n){
 
-        return Long.toBinaryString(n);
-    }
-
+  public static String int2bin(int n){
+    return Long.toBinaryString(n);
+  }
 }
